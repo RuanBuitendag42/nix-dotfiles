ea73c3587b8dbfe4b44116b8192a672f5ad51e08
Ruan Buitendag <ruan@minm.co.za>
2024-09-08 09:01:33 +0200

    fixed keymaps ...

--- ruanb/config/nvim/core/keymaps.nix
+++ ruanb/config/nvim/core/keymaps.nix
@@ -15,19 +15,19 @@
 
       ## Window Management ##
       {
-        actions = "<C-w>v";
+        action = "<C-w>v";
         key = "<leader>sv";
         mode = "n";
         options.desc = "Split windows vertiacally";
       }
       {
-        actions = "<C-w>s";
+        action = "<C-w>s";
         key = "<leader>sh";
         mode = "n";
         options.desc = "Split windows horizontally";
       }
       {
-        actions = "<C-w>=";
+        action = "<C-w>=";
         key = "<leader>se";
         mode = "n";
         options.desc = "Make splits equal size";
@@ -67,37 +67,37 @@
 
       ### TABS ###
       {
-        actions = "<cmd>close<CR>";
+        action = "<cmd>close<CR>";
         key = "<leader>sx";
         mode = "n";
         options.desc = "Close current split";
       }
       {
-        actions = "<cmd>tabnew<CR>";
+        action = "<cmd>tabnew<CR>";
         key = "<leader>to";
         mode = "n";
         options.desc = "Open new tab";
       }
       {
-        actions = "<cmd>tabclose<CR>";
+        action = "<cmd>tabclose<CR>";
         key = "<leader>tx";
         mode = "n";
         options.desc = "Close current tab";
       }
       {
-        actions = "<cmd>tabn<CR>";
+        action = "<cmd>tabn<CR>";
         key = "<leader>tn";
         mode = "n";
         options.desc = "Go to next tab";
       }
       {
-        actions = "<cmd>tab[]<CR>";
+        action = "<cmd>tab[]<CR>";
         key = "<leader>tp";
         mode = "n";
         options.desc = "Go to previous tab";
       }
       {
-        actions = "<cmd>tabnew %<CR>";
+        action = "<cmd>tabnew %<CR>";
         key = "<leader>tf";
         mode = "n";
         options.desc = "Open current buffer in new tab";
