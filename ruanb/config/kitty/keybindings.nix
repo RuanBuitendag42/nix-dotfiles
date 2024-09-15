c5bc7efee0726e74f614dae82ba366024a1c00cf
Ruan Buitendag <ruan@minm.co.za>
2024-09-15 20:30:21 +0200

    Nicer keybinding for kitty

--- ruanb/config/kitty/keybindings.nix
+++ ruanb/config/kitty/keybindings.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.kitty.settings = {
     "ctrl+shift+t" = "new_tab"; # Create a new tab
     "ctrl+shift+w" = "close_tab"; # Close the current tab
-    "ctrl+shift+tab" = "next_tab"; # Switch to the next tab
-    "ctrl+shift+iso_left_tab" = "previous_tab"; # Switch to the previous tab
+    "ctrl+shift+]" = "next_tab"; # Switch to the next tab
+    "ctrl+shift+[" = "previous_tab"; # Switch to the previous tab
     "ctrl+shift+r" = "rename_tab"; # Rename the current tab
     "ctrl+shift+," = "move_tab_left"; # Move the current tab to the left
     "ctrl+shift+." = "move_tab_right"; # Move the current tab to the right
