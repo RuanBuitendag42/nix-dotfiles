fcd5c0a20a4f87db5215f75d0dd42349475e1150
Ruan Buitendag <ruan@minm.co.za>
2024-10-01 20:45:23 +0200

    tmux / supermaven

--- ruanb/config/tmux/extraConfig.nix
+++ ruanb/config/tmux/extraConfig.nix
@@ -15,6 +15,26 @@
         set-option -g status-position top
 
         set -g renumber-windows on
+
+        # resize panes
+        bind-key -r H resize-pane -L 5
+        bind-key -r J resize-pane -D 5
+        bind-key -r K resize-pane -U 5
+        bind-key -r L resize-pane -R 4
+
+        # split panes using ctrl c/v
+        bind-key -r v split-window -v
+        bind-key -r c split-window -h
+
+        # move panes
+        bind-key -r 'H' select-pane -L
+        bind-key -r 'J' select-pane -D
+        bind-key -r 'K' select-pane -U
+        bind-key -r 'L' select-pane -R
+
+        # move windows
+        bind-key -r 'H' previous-window
+        bind-key -r 'L' next-window
       '';
   };
 }
