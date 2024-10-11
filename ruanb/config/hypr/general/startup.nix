30e17dc25061be34939caa0107ca48f92bb71d0e
Ruan Buitendag <ruan@minm.co.za>
2024-10-11 05:46:11 +0200

    Fixed wallpaper
    Small delay to swww init so that it has priority over stylix

--- ruanb/config/hypr/general/startup.nix
+++ ruanb/config/hypr/general/startup.nix
@@ -1,15 +1,15 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "swww init"
       "$browser & $terminal"
       "~/.config/waybar/scripts/launch.sh"
       "wl-paste --type text --watch cliphist store"
       "wl-paste --type image --watch cliphist store"
-      "gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha'"
-      "gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'"
-      "gsettings set org.gnome.desktop.interface font-name 'FiraCode Nerd Font Mono'"
-      "gsettings set org.gnome.desktop.interface cursor-theme 'Catppuccin-Mocha-Dark-Cursors'"
+      "sleep 1; swww-daemon"
+      # "gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha'"
+      # "gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'"
+      # "gsettings set org.gnome.desktop.interface font-name 'FiraCode Nerd Font Mono'"
+      # "gsettings set org.gnome.desktop.interface cursor-theme 'Catppuccin-Mocha-Dark-Cursors'"
       # "nm-applet &"
       # "waybar & hyprpaper & firefox"
     ];
