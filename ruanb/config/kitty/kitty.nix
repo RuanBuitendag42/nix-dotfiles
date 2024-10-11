baab97ae044d8c844cc1c2f6d3c00220636fa92e
Ruan Buitendag <ruan@minm.co.za>
2024-10-11 05:46:29 +0200

    Launch tmux on kitty terminal launch

--- ruanb/config/kitty/kitty.nix
+++ ruanb/config/kitty/kitty.nix
@@ -1,10 +1,16 @@
-{ config, pkgs, ... }:
-{
+{pkgs, ...}: let
+  startupSession = pkgs.writeText "startupSession" ''
+    launch tmux
+  '';
+in {
   imports = [
     ./appearance.nix
     ./keybindings.nix
   ];
   programs.kitty = {
     enable = true;
+    settings = {
+      startup_session = "${startupSession}";
+    };
   };
 }
