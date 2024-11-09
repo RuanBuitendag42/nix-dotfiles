a334f419d38dfb102200feec3ab02536b4e971be
Ruan Buitendag <ruan@minm.co.za>
2024-11-09 07:03:51 +0200

    Waybar ..

--- ruanb/config/scripts.nix
+++ ruanb/config/scripts.nix
@@ -1,9 +1,9 @@
 {
   home.file = {
-    ".config/waybar/scripts/launch.sh" = {
-      source = ./waybar/scripts/launch.sh;
-      executable = true;
-    };
+    # ".config/waybar/scripts/launch.sh" = {
+    #   source = ./waybar/scripts/launch.sh;
+    #   executable = true;
+    # };
     ".config/swww/scripts/changeWallpaper.sh" = {
       source = ./swww/changeWallpaper.sh;
       executable = true;
--- ruanb/config/waybar/waybar.nix
+++ ruanb/config/waybar/waybar.nix
@@ -1,9 +1,15 @@
-{
-  imports = [
-    ./settings.nix
-    ./colors.nix
-  ];
+{ config, ... }: {
+  # imports = [
+  # ./settings.nix
+  # ./colors.nix
+  # ];
   programs.waybar = {
     enable = true;
+  };
+  home.file = {
+    ".config/waybar/" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/ruanb/.config/nix-dotfiles/ruanb/config/waybar";
+      recursive = true;
+    };
   };
 }
